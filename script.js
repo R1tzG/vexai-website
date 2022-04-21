f5cf7dda642723cd48af88f292c8f6e89dccf909
Zephyrus011 <ritvikgupta011@gmail.com>
2022-04-20 20:24:53 -0400

    new style, finished layout

--- script.js
+++ script.js
@@ -2,9 +2,27 @@ var body = document.getElementsByTagName('body')[0];
 
 var popup  = document.getElementById('popup');
 
-var button = document.getElementById('nav-element');
+var popup2 = document.getElementById('popup2');
+
+var popup3 = document.getElementById('popup3');
+
+var popup4 = document.getElementById('popup4');
+
+var button = document.getElementById('nav-element1');
+
+var button2 = document.getElementById('nav-element2');
+
+var button3 = document.getElementById('nav-element3');
+
+var button4 = document.getElementById('nav-element4');
 
 var span = document.getElementById('close');
+
+var span2 = document.getElementById('close2');
+
+var span3 = document.getElementById('close3');
+
+var span4 = document.getElementById('close4');
 
 button.onclick = function() {
     popup.style.visibility = 'visible';
@@ -13,5 +31,35 @@ button.onclick = function() {
 
 span.onclick = function() {
     popup.style.visibility = 'hidden';
+    body.style.overflow = 'auto';
+}
+
+button2.onclick = function() {
+    popup2.style.visibility = 'visible';
+    body.style.overflow = 'hidden';
+}
+
+span2.onclick = function() {
+    popup2.style.visibility = 'hidden';
+    body.style.overflow = 'auto';
+}
+
+button3.onclick = function() {
+    popup3.style.visibility = 'visible';
+    body.style.overflow = 'hidden';
+}
+
+span3.onclick = function() {
+    popup3.style.visibility = 'hidden';
+    body.style.overflow = 'auto';
+}
+
+button4.onclick = function() {
+    popup4.style.visibility = 'visible';
+    body.style.overflow = 'hidden';
+}
+
+span4.onclick = function() {
+    popup4.style.visibility = 'hidden';
     body.style.overflow = 'auto';
 }
